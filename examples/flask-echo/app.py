0a9cd3fd84fad7bf2fd7af8f18e37f02d8841b83
marc lihan <marc.lihan@linecorp.com>
2017-01-01 01:21:35 +0800

    add TemplateSendMessage

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -26,7 +26,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage
 )
 
 app = Flask(__name__)
