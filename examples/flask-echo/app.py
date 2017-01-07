d23b4c32dfed75cdb6fcdba1791d3d0b1e5cc852
marc lihan <marc.lihan@linecorp.com>
2017-01-07 22:37:28 +0800

    change logic

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -105,7 +105,7 @@ def callback():
 
             isCelebrity = len(classifiers) > 1
             celeb_confidence = classifiers[0]['classes'][0]['score']
-            isPerson = 'person' in json.dumps(classifiers) and celeb_confidence > 0.6
+            isPerson = 'person' in json.dumps(classifiers) or celeb_confidence > 0.6
             
             app.logger.info('isCelebrity: ' + str(isCelebrity) + ' isPerson:' + str(isPerson) + ' confidence:' + str(celeb_confidence) )
 
