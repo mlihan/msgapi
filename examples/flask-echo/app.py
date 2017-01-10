89cdcd560b7bc9acf7fcb082e16eaecc84f0b246
marc lihan <marc.lihan@linecorp.com>
2017-01-10 21:52:36 +0800

    change to https

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -394,13 +394,13 @@ def createRedCarpet(data):
     url = ''
     # setup url based on gender
     if gender == 'female':
-        url = 'http://res.cloudinary.com/{0}/image/upload/c_thumb,' \
+        url = 'https://res.cloudinary.com/{0}/image/upload/c_thumb,' \
         'g_face:center,h_100,r_max,w_70/u_bradgelina,x_-20,y_290/v1484046371/' \
         '{1}.jpg'.format(cloudinary_cloud, sender_img_id)
     else:
-        url = 'http://res.cloudinary.com/dmshmeaoc/image/upload/c_thumb,' \
+        url = 'https://res.cloudinary.com/{0}/image/upload/c_thumb,' \
         'g_face:center,h_90,r_max,w_60/a_-10/u_bradgelina,x_110,y_310/v1484046371/' \
-        '{0}.jpg'.format(sender_img_id)
+        '{1}.jpg'.format(cloudinary_cloud,sender_img_id)
     template = ImageSendMessage(
         original_content_url=url,
         preview_image_url=url
