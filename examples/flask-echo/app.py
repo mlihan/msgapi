0801280695e30dd15a4e34145df267889f84bcf4
marc lihan <marc.lihan@linecorp.com>
2017-01-12 02:31:53 +0800

    update count

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -238,7 +238,8 @@ def getMessageForClassifier(classifiers, sender_image_id=None):
     # 1 a person and celebrity look alike, send a template message carousel
     if isCelebrity and gender is not None:
         app.logger.info('[MATCH FOUND]')
-        updateBluemixKey(((bluemix_index + 1) % 4) + 1)
+        bluemix_index = (((bluemix_index + 1) % 4) + 1)
+        updateBluemixKey(bluemix_index)
         return createMessageTemplate(sorted_list, gender, age, 3, sender_image_id)
     # 2 a celebrity lookalike but not a person
     elif isCelebrity:
